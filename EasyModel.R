b5885bb9e9b45bf702910a1f76c2e2dd081c8023
U-NDC\ptbritto <paul.t.britton@gmail.com>
2014-06-03 17:03:20 -0500

    began work on using environments

--- EasyModel.R
+++ EasyModel.R
@@ -10,19 +10,17 @@ rm(list=ls())			# removes all objects from the current
 
 source("PUPPIES.R")
 
-#VerboseLevel <- 3	# default = 1, choose from 0 to 3
-N <- 555
-print(paste("Number of Samples =",N))
-seed <- 1		# global variable
+VerboseLevel <- 3	# default = 1, choose from 0 to 3
+#seed <- 1		# global variable
 
 # samples from Vehicle system distributions,Engine Param, Alpha Factors
-X <- montecarlo(N,"parameters.pp")
+X <- montecarlo(N=5,"INPUTS/new-pp.pp")
 # propagate uncertainty into vehicle models
-Y <- propagate("equations.pe",X)
-Z <- cbind(Y,X)	# combine (column-wise) Y and X
-scatterbars(plotname="OUTPUTS/Easyplot.tiff",Data=Z,filter="*4|S*")
+#Y <- propagate("equations.pe",X)
+#Z <- cbind(Y,X)	# combine (column-wise) Y and X
+scatterbars(plotname="OUTPUTS/Easyplot.tiff",Data=X,filter="M?|S?")
 
-scatterbars(plotname="OUTPUTS/OneEasyplot.tiff",Data=Z,
-	desc="INPUTS/Easy.pd",filter="S4")
+#scatterbars(plotname="OUTPUTS/OneEasyplot.tiff",Data=Z,
+#	desc="INPUTS/Easy.pd",filter="S4")
 
 print("EasyModel.R Complete")	# status print statement demonstration
--- Modules/MonteCarlo.R
+++ Modules/MonteCarlo.R
@@ -24,72 +24,26 @@
 #
 
 # constant vector
-cD <- function(N,P) rep(as.numeric(P$a),times=N)
+#consV <- function(c=0) rep(c,times=N)
 
 # uniform distribution
-uD <- function(N,P) runif(N,as.numeric(P$a),as.numeric(P$b))
+unifD <- function(a=0,b=1) runif(N,a,b)
 
 # create a vector of N trianglular samples
 # a = min
-# c = mode
-# b = max
-tD <- function(N,P) {
-	a <- as.numeric(P$a)
-	c <- as.numeric(P$b)
-	b <- as.numeric(P$c)
-	if (!((0<=a)&&(a<=c)&&(c<=b)&&(b<=1))) {
+# b = mode
+# c = max
+triaD <- function(a,b,c) {
+	if (!((0<=a)&&(a<=b)&&(b<=c)&&(c<=1))) {
 		stop(paste(row.names(P),"tD:",
-			"Invalid Triangle Distribution Parameters"))
+			"Invalid Triangle Districution Parameters"))
 	}
 	U <- runif(N,0,1)
-	x <- (c-a)/(b-a)
+	x <- (b-a)/(c-a)
 	Tri <- U <= x
-	Tri[Tri==TRUE] <- a+sqrt(U[U<=x]*(b-a)*(c-a))
-	Tri[Tri==FALSE] <- b-sqrt((1-U[U>x])*(b-a)*(b-c))
+	Tri[Tri==TRUE] <- a+sqrt(U[U<=x]*(c-a)*(b-a))
+	Tri[Tri==FALSE] <- c-sqrt((1-U[U>x])*(c-a)*(c-b))
 	return(Tri)
-}
-
-# N beta samples
-bD <- function(N,P) {
-	a<- as.numeric(P$a)
-	b<- as.numeric(P$b)
-	return(rbeta(N,a,b))
-}
-
-# N gamma samples
-gD <- function(N,P) {
-	shape <- as.numeric(P$a)
-	rate <- as.numeric(P$b)
-	return(rgamma(N,shape,rate))
-}
-
-# create a vector of N lognormal samples
-# MED = median
-# EF = 95th/50th
-lD <- function(N,P) {
-	MED <- as.numeric(P$a)
-	EF <- as.numeric(P$b)
-	mu <- log(MED)
-	sigma <- log(EF)/qnorm(0.95)
-	return(rlnorm(N,mu,sigma))
-}
-
-####################################################
-# create a vector of N samples based on an emperical distribution
-# (first fit a cdf based on data then spline fit the inverse cdf)
-eD <- function(N,P) {
-	empfile <- as.character(P$f)
-	if (VerboseLevel >= 2) print(paste("Loading Emperical Data:",empfile))
-	res <- 10*N + 1
-	X <- seq(0,1,length.out=res)
-	if (file.access(empfile,mode=4)) {
-		stop(paste("File access error:",empfile))
-	}
-	D <- scan(empfile)
-	Qfun <- splinefun(ecdf(D)(X),X)
-	if (VerboseLevel >= 2) print(paste("Piece-wise spline fit complete:",
-					empfile))
-	return(Qfun(runif(N,0,1)))
 }
 
 ##################################################
@@ -98,76 +52,35 @@ eD <- function(N,P) {
 #
 
 
-#######################
-# ? Load data from file and apply it directly in the simulation
-#DATA <- function(Z,P) scan(as.character(P$f))
-
 #################
-# Logical operations
+# Advanced Logical operation filters for probabilities
 #
 
-# logical OR operation for compound event distributions
-OR <- function(Z,P) {
-	filter <- as.character(P$f)
-	class(filter) <- wildcardclass
-	X <- filterdata(torx(filter),Z)
-	return(1-apply(1-X,1,prod))
-}
-
-# logical AND operation for compound event distributions
-AND <- function(Z,P) {
-	filter <- as.character(P$f)
-	class(filter) <- wildcardclass
-	X <- filterdata(torx(filter),Z)
-	return(apply(X,1,prod))
-}
-
-#################
-# Mathematical operations <- replace with EXPR
-#
-
-# multiply by constant: a*X
-#MULT <- function(Z,P) {
-#	a <- as.numeric(P$a)
-#	X <- Z[[as.character(P$f)]]
-#	return(a*X)
+# logical OR operation filter for probabilities
+#OR <- function(filter) {
+#	class(filter) <- wildcardclass
+#	X <- filterdata(torx(filter),Z)
+#	return(1-apply(1-X,1,prod))
 #}
 
-# add constant: X+b
-#ADD <- function(Z,P) {
-#	b <- as.numeric(P$b)
-#	X <- Z[[as.character(P$f)]]
-#	return(X+b)
+# logical AND operation filter for probabilities
+#AND <- function(filter) {
+#	class(filter) <- wildcardclass
+#	X <- filterdata(torx(filter),Z)
+#	return(apply(X,1,prod))
 #}
 
-# raise to power: X^c
-#POW <- function(Z,P) {
-#	c <- as.numeric(P$c)
-#	X <- Z[[as.character(P$f)]]
-#	return(X^c)
-#}
-
-# Note: this TRAN is trumped by EXPR below.  Additionally, TRAN is used
-# represent Transfer Gate in the PUPPIES fault tree format.
-# Apply the transformation: T(X)=a*X^c+b
-#TRAN <- function(Z,P) {
-#	a <- as.numeric(P$a)
-#	b <- as.numeric(P$b)
-#	c <- as.numeric(P$c)
-#	X <- Z[[as.character(P$f)]]
-#	return(a*X^c+b)
-#}
 
 #######################################
 #
 # evaluate an expressions within the parameter file 
 #
-EXPR <- function(Z,P) {
-	EXPRdf <- as.data.frame(list(expression=as.character(P$f)))
-	row.names(EXPRdf) <- list("ExpressionString")
-	ExprResult <- propagate3(EXPRdf,Z)
-	return(ExprResult[,1])
-}
+#EXPR <- function(Z,P) {
+#	EXPRdf <- as.data.frame(list(expression=as.character(P$f)))
+#	row.names(EXPRdf) <- list("ExpressionString")
+#	ExprResult <- propagate3(EXPRdf,Z)
+#	return(ExprResult[,1])
+#}
 
 #################
 # access script information
@@ -183,23 +96,23 @@ EXPR <- function(Z,P) {
 
 # call the function f that matches the string 'func'.
 # The remaining arguments are passed to f.
-CallFunc <- function(func,...) {
-	if (VerboseLevel == 3) {
-		dots <- list(...)
-		print("Inside CallFunc for:")
-		print(dots[2])
-	}
-	fun <- as.character(func)
-	if (VerboseLevel == 3) {
-		print(paste("Attempting to find function:",fun))
-	}
-	f <- match.fun(fun)
-	if (VerboseLevel == 3) {
-		print(paste("Exiting CallFunc, ",
-		 "and attempting to call the function: ",fun,sep=""))
-	}
-	return(f(...))
-}
+#CallFunc <- function(func,...) {
+#	if (VerboseLevel == 3) {
+#		dots <- list(...)
+#		print("Inside CallFunc for:")
+#		print(dots[2])
+#	}
+#	fun <- as.character(func)
+#	if (VerboseLevel == 3) {
+#		print(paste("Attempting to find function:",fun))
+#	}
+#	f <- match.fun(fun)
+#	if (VerboseLevel == 3) {
+#		print(paste("Exiting CallFunc, ",
+#		 "and attempting to call the function: ",fun,sep=""))
+#	}
+#	return(f(...))
+#}
 
 # UDist need to eliminate this in order to allow for more flexibility
 # in applying multiple missions times to the same failure rate sample
@@ -207,25 +120,25 @@ CallFunc <- function(func,...) {
 # Generate uncertainty distributions from parameter data.
 # The function along with CallFunc act like a switchboard
 # from the parameter data inputfile to the uncertainty distribution functions
-UDist <- function(N,P) {
-	S <- CallFunc(P$dist,N,P)
-	X <- switch(as.character(P$calc),
-		I = S,			# identity: use samples as is
-		e = { if (P$t == 0) warning(paste(row.names(P),": t = 0"))
-		pexp(P$t,S) },	# constant rate: apply exponential cdf
-		w = { if (P$t == 0) warning(paste(row.names(P),": t = 0"))
-		pweibull(P$t,P$c,1/S) } # variable rate: apply weibull cdf
-	)
-	return(X)
-}
+#UDist <- function(N,P) {
+#	S <- CallFunc(P$dist,N,P)
+#	X <- switch(as.character(P$calc),
+#		I = S,			# identity: use samples as is
+#		e = { if (P$t == 0) warning(paste(row.names(P),": t = 0"))
+#		pexp(P$t,S) },	# constant rate: apply exponential cdf
+#		w = { if (P$t == 0) warning(paste(row.names(P),": t = 0"))
+#		pweibull(P$t,P$c,1/S) } # variable rate: apply weibull cdf
+#	)
+#	return(X)
+#}
 
-montecarlo <- function(N,filter) {
+montecarlo_batch <- function(N,filter) {
 	if (VerboseLevel > 0) print(paste("montecarlo() matching:",filter))
 	class(filter) <- wildcardclass
 	LF <- list.files(pattern=torx(filter),recursive=TRUE,full.names=TRUE)
 	for (i in LF) {
 		if (VerboseLevel > 0) print(paste("montecarlo() found:",i))
-		newone <- montecarlo2(N,i)
+		newone <- montecarlo(N,i)
 		if (exists("X")) X <- cbind(newone,X[!(X %in% newone)])
 		else X <- newone
 	}
@@ -233,45 +146,114 @@ montecarlo <- function(N,filter) {
 	return(X)
 }
 
-montecarlo2 <- function(N,pfile) {
-	if (VerboseLevel > 0) print(paste("montecarlo() reading:",pfile))
-	if (file.access(pfile,mode=4)) {
-		stop(paste("File access error:",pfile))
+montecarlo_read <- function(mfile) {
+	if (VerboseLevel > 0) print(paste("montecarlo_read() reading:",mfile))
+	if (file.access(mfile,mode=4)) {
+		stop(paste("File access error:",mfile))
 	}
-	Params <- read.csv(pfile,sep=args$fieldsep,row.names=1)
-	if (VerboseLevel > 0) print(Params)
-	Z <- montecarlo3(N,Params)
-	if (VerboseLevel >= 2) print(paste("montecarlo() complete on:",pfile))
-	return(Z)
+	modeldef <- read.csv(mfile,sep=args$fieldsep,quote=NULL)#,row.names=1)
+#	if (VerboseLevel > 0) print(modeldef)
+#	Z <- montecarlo3(N,modeldef)
+#	if (VerboseLevel >= 2) print(paste("montecarlo() complete on:",mfile))
+print(tail(modeldef,-1))
+	return(tail(modeldef,-1))
 }
 
 # sample the distributions defined by the parameter data
-montecarlo3 <- function(N,Params)
+montecarlo <- function(N,mfile)
 {
-	Params <- rowrevorder(Params)
+# N beta samples
+betaD <- function(alpha,beta) {
+	return(rbeta(N,alpha,beta))
+}
+
+# N gamma samples
+gammD <- function(shape,rate) {
+	return(rgamma(N,shape,rate))
+}
+
+# create a vector of N lognormal samples
+# median = 50th
+# EF = 95th/50th
+lognD <- function(median,EF) {
+	mu <- log(median)
+	sigma <- log(EF)/qnorm(0.95)
+	return(rlnorm(N,mu,sigma))
+}
+
+####################################################
+# create a vector of N samples based on an emperical distribution
+# (first fit a cdf based on data then spline fit the inverse cdf)
+empeD <- function(empfile) {
+	if (VerboseLevel >= 2) print(paste("Loading Emperical Data:",empfile))
+	res <- 10*N + 1
+	X <- seq(0,1,length.out=res)
+	if (file.access(empfile,mode=4)) {
+		stop(paste("File access error:",empfile))
+	}
+	D <- scan(empfile)
+	Qfun <- splinefun(ecdf(D)(X),X)
+	if (VerboseLevel >= 2) print(paste("Piece-wise spline fit complete:",
+					empfile))
+	return(Qfun(runif(N,0,1)))
+}
+
+	# save R language defaults to keep functionality for
+	# if-statements, etc. (?forced to do this because
+	# locally defined functions seem to persist globally!)
+	defaultOR <- '|'
+	defaultAND <- '&'
+	# overload logical operators to operate on
+	# probability distrubutions
+	'|' <- ORgate
+	'&' <- ANDgate
+
+	modeldef <- rowrevorder(montecarlo_read(mfile))
+print(modeldef)
 	# define and initialize the data structure that will
 	# contain the vectors of monte carlo samples for the
 	# parameters
-	Z <- as.data.frame(array(0,c(N,nrow(Params))))
-	names(Z) <- row.names(Params)
+	size <- nrow(modeldef)
+print(size)
+	Z <- as.data.frame(array(0,c(N,size)))
+#	names(Z) <- row.names(Params)
+	e <- new.env()
+	e$N <- N
+	for (i in 1:size) {
+print(modeldef[[i,1]])
+print(modeldef[[i,2]])
+#	Z <- as.data.frame(as.list(e))
+		code <- parse(text=as.character(modeldef[[i,2]]))
+		assign(as.character(modeldef[[i,1]]),eval(code,e),e)
+	}
 
 	# consider replacing switch statement with generic functions
 	#
 	# sample uncertainty distributions from parameter data
-	for (i in names(Z)) {
-		P <- Params[i,]
-		Z[i] <- switch(as.character(P$dist),
-			CF={ j <- match(i,names(Z))
-			#call function
-			CallFunc(P$calc,Z[1:ifelse(j>1,j-1,j)],P)},
-			#uncertainty distribution
-			{ if (!is.null(seed)) {
-				if (VerboseLevel == 3) {
-					print(paste("Setting seed =",seed))
-				}
-				set.seed(seed)		#set seed
-				seed <- seed + 1}	#proactively update seed
-			UDist(N,P)})			#uncertainty dist
-	}
+#	for (i in names(Z)) {
+#		P <- Params[i,]
+#		Z[i] <- switch(as.character(P$dist),
+#			CF={ j <- match(i,names(Z))
+#			#call function
+#			CallFunc(P$calc,Z[1:ifelse(j>1,j-1,j)],P)},
+#			#uncertainty distribution
+#			{ if (!is.null(seed)) {
+#				if (VerboseLevel == 3) {
+#					print(paste("Setting seed =",seed))
+#				}
+#				set.seed(seed)		#set seed
+#				seed <- seed + 1}	#proactively update seed
+#			UDist(N,P)})			#uncertainty dist
+#	}
+#	return(revorder(Z))
+	# explicity remove overloaded operators
+	rm('|')
+	rm('&')
+	# restore R language defaults
+	'|' <- defaultOR
+	'&' <- defaultAND
+	print(as.list(e))
+	Z <- as.data.frame(as.list(e))
+	print(Z)
 	return(revorder(Z))
 }
