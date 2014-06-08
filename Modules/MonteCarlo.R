3146b123a7f0a497c6cafd27a62a3c272548bb8c
PaulTBritton <paul.t.britton@gmail.com>
2014-06-07 23:32:42 -0500

    added clonepm()

--- Modules/MonteCarlo.R
+++ Modules/MonteCarlo.R
@@ -7,16 +7,6 @@
 ###################################
 
 
-#montecarlo_read <- function(mfile) {
-#	if (VerboseLevel > 0) print(paste("montecarlo_read() reading:",mfile))
-#	if (file.access(mfile,mode=4)) {
-#		stop(paste("File access error:",mfile))
-#	}
-#	modeldef <- read.csv(mfile,sep=args$fieldsep,quote=NULL)#,row.names=1)
-#	if (VerboseLevel > 0) print(modeldef)
-#	return(modeldef)
-#}
-
 # evaluate a PUPPIES model with N random iteration
 evalpm <- function(N,seed=NULL,pmname="PUPPIES Model",
 		pmfile="",mexpr=parse(file=pmfile))
@@ -41,27 +31,31 @@ appendpm <- function(pm,pmfile="",mexpr=parse(file=pmfile)) {
 	eval(mexpr,pm$r)
 }
 
+clonepm <- function(pm) {
+	e <- as.environment(as.list(pm$r,all.names=TRUE))
+	parent.env(e) <- parent.env(pm$r)
+	return(list(n=pm$n,r=e))
+}
+
 # propagate results from from one PUPPIES model into
 # another PUPPIES model and return the only the new results
 spawnpm <- function(pm,pmname="PUPPIES Model",pmfile="",
 		mexpr=parse(file=pmfile)) {
-	e <- as.environment(as.list(pm$r,all.names=TRUE))
-	parent.env(e) <- parent.env(pm$r)
-	eval(mexpr,e)
-	rm(list=ls(pm$r),envir=e)
-	return(list(n=pmname,r=e))
+	e <- clonepm(pm)
+	eval(mexpr,e$r)
+	rm(list=ls(pm$r),envir=e$r)
+	return(list(n=pmname,r=e$r))
 }
 
 batch_evalpm <- function(N,seed=NULL,pmname="PUPPIES Model",filter) {
 	if (VerboseLevel > 0) print(paste("batch_evalpm() matching:",filter))
 	class(filter) <- wildcardclass
 	LF <- list.files(pattern=torx(filter),recursive=TRUE,full.names=TRUE)
+	if (VerboseLevel > 0) print(paste("batch_evalpm() found:",LF[1]))
 	model <- evalpm(N,seed,pmname,pmfile=LF[1])
 	for (i in LF[-1]) {
 		if (VerboseLevel > 0) print(paste("batch_evalpm() found:",i))
 		appendpm(pm=model,pmfile=i)
-#		if (exists("X")) X <- cbind(newone,X[!(X %in% newone)])
-#		else X <- newone
 	}
 	if (VerboseLevel >= 2) print(paste("batch_evalpm() complete on:",filter))
 	return(model)
@@ -75,4 +69,5 @@ batch_appendpm <- function(pm,filter) {
 		if (VerboseLevel > 0) print(paste("batch_appendpm() found:",i))
 		appendpm(pm,i)
 	}
+	if (VerboseLevel >= 2) print(paste("batch_appendpm() complete on:",filter))
 }
